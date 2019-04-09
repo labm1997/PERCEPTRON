5c7b85a11541c92763a814148b63ddaf054f1d96
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-04-09 20:23:15 -0300

    Implemented data extraction

--- src/main.py
+++ src/main.py
@@ -1,1 +1,31 @@
 # Programa para treinar um perceptron.
+
+# Variáveis de controle
+MINE = 1
+ROCK = 0
+
+# Extração dos dados:
+with open("./data/sonar.train-data", "r") as train_file:
+
+    # Primeiro, vamos ler cada linha, separando os elementos de uma linha por
+    # vírgula em um array e retirando os '\n'.
+    lines = list(map(lambda x: x.replace('\n', '').split(','), train_file.readlines()))
+
+    # Depois, extrair os inputs (Tudo menos a última posição).
+    train_features = list(map(lambda x: x[:-1], lines))
+
+    # Finalmente, extrair o label conforme o valor da última posição.
+    train_labels = list(map(lambda x: MINE if(x[-1] == 'M') else ROCK, lines))
+
+with open("./data/sonar.test-data", "r") as test_file:
+
+    # Primeiro, vamos ler cada linha, separando os elementos de uma linha por
+    # vírgula em um array e retirando os '\n'.
+    lines = list(map(lambda x: x.replace('\n', '').split(','), test_file.readlines()))
+
+    # Depois, extrair os inputs (Tudo menos a última posição).
+    test_features = list(map(lambda x: x[:-1], lines))
+
+    # Finalmente, extrair o label conforme o valor da última posição.
+    test_labels = list(map(lambda x: MINE if(x[-1] == 'M') else ROCK, lines))
+
