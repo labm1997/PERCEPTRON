7563ce6adc30fbb53a5237b5e7578939b95e1681
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-04-10 21:04:09 -0300

    Data extraction and weight generation overhaul *Refactored the behavior for data extraction in a function. *Casted each feature reading to the float data type.

--- src/main.py
+++ src/main.py
@@ -3,34 +3,33 @@
 # Pacotes importados:
 from random import random
 
-# Variáveis de controle:
-MINE = 1
-ROCK = 0
+# Funções auxiliares:
+def extract_sonar_data(file_path, mine_value=1, rock_value=0):
+
+# TODO idea: Replace these lambdas to improve readability!
+
+    with open(file_path, "r") as file:
+        # Primeiro, vamos ler cada linha, separando os elementos de uma linha por
+        # vírgula em um array e retirando os '\n'.
+        lines = list(map(lambda x: x.replace('\n', '').split(','),
+                         file.readlines()))
+
+        # Depois, extrair os inputs como floats (Tudo menos a última posição).
+        features = list(map(lambda x: [float(item) for item in x[:-1]], lines))
+
+        # Append de bias para última posição de cada conjunto de features.
+        for entry in features:
+            entry.append(1.0)
+
+        # Finalmente, extrair o label conforme o valor da última posição.
+        labels = list(map(lambda x: mine_value if(x[-1] == 'M') else rock_value,
+                          lines))
+
+    return features, labels
 
 # Extração dos dados:
-with open("./data/sonar.train-data", "r") as train_file:
-
-    # Primeiro, vamos ler cada linha, separando os elementos de uma linha por
-    # vírgula em um array e retirando os '\n'.
-    lines = list(map(lambda x: x.replace('\n', '').split(','), train_file.readlines()))
-
-    # Depois, extrair os inputs (Tudo menos a última posição).
-    train_features = list(map(lambda x: x[:-1], lines))
-
-    # Finalmente, extrair o label conforme o valor da última posição.
-    train_labels = list(map(lambda x: MINE if(x[-1] == 'M') else ROCK, lines))
-
-with open("./data/sonar.test-data", "r") as test_file:
-
-    # Primeiro, vamos ler cada linha, separando os elementos de uma linha por
-    # vírgula em um array e retirando os '\n'.
-    lines = list(map(lambda x: x.replace('\n', '').split(','), test_file.readlines()))
-
-    # Depois, extrair os inputs (Tudo menos a última posição).
-    test_features = list(map(lambda x: x[:-1], lines))
-
-    # Finalmente, extrair o label conforme o valor da última posição.
-    test_labels = list(map(lambda x: MINE if(x[-1] == 'M') else ROCK, lines))
+train_features, train_labels = extract_sonar_data("./data/sonar.train-data")
+test_features, test_labels = extract_sonar_data("./data/sonar.test-data")
 
 # Inicialização dos pesos:
 weights = [(random() - 0.5) * 0.2 for i in range(len(test_features) + 1)]
